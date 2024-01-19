2f3c98658daf2810a3bfed8822da8b6214780001
diersentre <diersentre@urbandaleschools.com>
2024-01-19 09:06:29 -0600

    Team4

---
+++ functiontest.py
@@ -0,0 +1,20 @@
+# import random
+# def volume_cylinder(r,h):
+#     ''' This function determines the volume of a cylinder'''
+#     vol = 3.14*r**2*h
+#     return vol
+#
+#  def main():
+#      volume_cylinder(1,5)
+# # volume = volume_cylinder(1,5)
+# # print(volume*6)
+# # print(help(volume_cylinder))
+#
+# if __name__ == "__main__":
+#     main()
+
+def hyp(leg1,leg2):
+    leg3 = ((leg1**2)+(leg2**2))//2
+    print(leg3)
+
+hyp(5,5)
